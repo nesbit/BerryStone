f419ff120d031d8c3268c3c594c106753a558c2c
nesbit <ethandburrell@gmail.com>
2016-05-30 22:49:50 -0700

    Update main.py

--- main.py
+++ main.py
@@ -21,39 +21,39 @@ def startBeacon(uri):
 	for s in range(len(schemes)):
 		thisScheme = schemes[s]
 		if uri.startswith(thisScheme):
-			print (thisScheme + "," + str(s))
+			#print (thisScheme + "," + str(s))
 			urlScheme = hex(s)[2:].zfill(2)
-	print (uri)
+	#print (uri) For debugging
 	#this makes the Expansion
 	while i < len(uri) and foundExpansion== False:
 		if uri[i] == '.':
 			for e in range(len(expansions)):
 				thisExpansion = expansions[e]
 				if uri.startswith(thisExpansion, i) and foundExpansion==False:
-					print ("yes " + thisExpansion + " " + str(e))
+					#print ("yes " + thisExpansion + " " + str(e))
 					thisE = e
 					foundExpansion = True
 		i = i + 1
-	#this puts the domain name in
+	#this puts the domain name in the string in hex
 	schemeLength = len(thisScheme)
 	expansionLength = len(thisExpansion)
 	uriString = uri[schemeLength:len(uri)-expansionLength]
-	print (uriString)
+	#print (uriString)
 	
 	while k < len(uriString):
 		uriHex=str(uriHex + " "+"{0:02x}".format(ord(uriString[k])))
 		k=k+1
 	#	print ("debug " +str(k) + uriHex)
-	#chr() converts int to hex
+	#Assembles the hex to send out
 	str1 = "02 01 1a 03 03 aa fe "+str(hex(len(uriString)+7)[2:].zfill(2))+" 16 aa fe "
 	str2 = "10 ed " +urlScheme+""+ uriHex+" "+hex(thisE)[2:].zfill(2)
 	str3 = str1 + str2
-	print ("Length " + str(len(re.findall(r'\w+', str3))))
+	#print ("Length " + str(len(re.findall(r'\w+', str3))))
 	
 	str3 = hex(len(re.findall(r'\w+', str3)))[2:].zfill(2) +" "+ str3
-	while len(re.findall(r'\w+', str3)) < 32: str3 = str3 + " 00"
+	while lepn(re.findall(r'\w+', str3)) < 32: str3 = str3 + " 00"
 	#str3 = str3.replace("\\x", " ")
-	print ("Hello " + str3)
+	#print ("Hello " + str3)
 	#print (int(str3))
 	os.system("sudo hcitool -i hci0 cmd 0x08 0x000a 00")
 #Set message
@@ -61,5 +61,5 @@ def startBeacon(uri):
 #Resume advertising
 	os.system("sudo hcitool -i hci0 cmd 0x08 0x000a 01")
 #def broadcast(message):
-
+#chr() converts int to hex
 startBeacon("https://ert.com")
