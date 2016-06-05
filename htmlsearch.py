4f99cdbbed217a821f9d45829301726fb999d5f4
nesbit <ethandburrell@gmail.com>
2016-06-04 22:36:45 -0700

    Update htmlsearch.py

--- htmlsearch.py
+++ htmlsearch.py
@@ -1,15 +1,22 @@
 import glob
-print glob.glob("*.html")
+#print glob.glob("*.html")
 arr = glob.glob("*.html")
 i=0
+ll=0
 k=[]
 ray =[]
+str=""
 while i < len(arr):
     file = open(arr[i], "r")
     #print file.read()
     k.append(file.read())
     i = i+1
-print k
+
+while ll<len(k[0]):
+    if k[0][ll] == "=":
+        str1 = k[0]
+        print str1[ll+1:len(k[0])]
+    ll = ll + 1
 '''
 Outputs:
 print print glob.glob("*.html")
